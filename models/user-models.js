f90fc0a7b8408423e79a9a89d79ea8cd90fc8c68
NamKhanh321 <129375652+NamKhanh321@users.noreply.github.com>
2025-07-17 18:43:43 +0700

    Update user-models.js

--- models/user-models.js
+++ models/user-models.js
@@ -35,7 +35,7 @@ userSchema.pre('save', async function(next) {
 });
 
 userSchema.methods.createJWT = function() {
-    return jwt.sign({userId: this._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
+    return jwt.sign({userId: this._id, role: this.role}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
 }
 
 userSchema.methods.checkPassword = async function(password) {

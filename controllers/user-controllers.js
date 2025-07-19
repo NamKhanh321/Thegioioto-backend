0bce052fe04475c8eaa8785a08ba6c2b196e218b
NamKhanh321 <129375652+NamKhanh321@users.noreply.github.com>
2025-07-20 01:21:26 +0700

    add register

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,35 +1,62 @@
 import User from '../models/user-models.js';
 import asyncWrapper from '../middlewares/asyncWrapper.js';
+import { BadRequestError, UnauthenticatedError, NotFoundError} from '../errors/index.js';
+import { StatusCodes } from 'http-status-codes';
 
+export const getAllUsers = asyncWrapper(async (req,res,next) => {
+    const users = await User.find({}).select('-password');
+    return res.status(StatusCodes.OK).json(users);
+})
 
-export const getAllUsers = (req,res,next) => {
-    res.json({message: "user info"});
-}
-
-export const getUsersById = function(req,res,next) {
+export const getUsersById = asyncWrapper(async (req,res,next) => {
     const {id} = req.params;
-    res.json({message: `user with id: ${id} info`});
-}
+    const user = await User.find({_id: id}).select('-password');
+    if(!user)
+        throw new NotFoundError('Không tìm thấy người dùng này');
+    return res.status(StatusCodes.OK).json(user);  
+})
 
 export const createUsers = asyncWrapper(async (req,res,next) => {
     const {username, password, role, name} = req.body;
     const existUser = await User.findOne({username});
     if(existUser){
-        return res.status(400).json({message: 'username is already taken'});
+        throw new BadRequestError('Tên người dùng đã tồn tại');
     }
 
     const user = await User.create({username, password, role, name});
-    if(user)
-    {
-        return res.status(201).json({message: "user created"});
-    }
-    return res.status(500).json({message: 'failed to create user'});
+    if(!user)
+        throw new BadRequestError('Tạo user thất bại!, thông tin không hợp lệ');
+    return res.status(StatusCodes.CREATED).json({msg: "Tạo user thành công"});
 } )
 
-export const updateUsers = function(req,res,next) {
-    res.json({message: "user updated"});
-}
+export const updateUsers = asyncWrapper(async (req,res,next) => {
+    const {id} = req.params;
+    const user = await User.findOne({_id: id});
+    // const user = await User.findOneAndUpdate({_id: id}, {password, name, role});
+    if(!user)
+        throw new NotFoundError('Không tìm thấy người dùng này');
+    const {password, name, role} = req.body;
+    if(password)
+    {
+        user.password = password;
+    }
+    user.name = name || user.name;
+    user.role = role || user.role;
+    const updatedUser = await user.save();
+    res.status(StatusCodes.OK).json(updatedUser);
+})
 
-export const deleteUsers = function(req,res,next) {
-    res.json({message: "user deleted"});
-}
+export const deleteUsers = asyncWrapper(async (req,res,next) => {
+    const {id} = req.params;
+    const user = await User.findOne({_id: id});
+    if(!user)
+        throw new NotFoundError('Không tìm thấy người dùng này');
+    if(user.role === 'admin')
+    {
+        const adminCount = User.countDocuments({role: 'admin'});
+        if(adminCount <= 1)
+            throw new BadRequestError('không thể xóa admin cuối cùng trong hệ thống');
+    }
+    await user.deleteOne();
+    res.status(StatusCodes.OK).json('Xóa người dùng thành công!');
+})
